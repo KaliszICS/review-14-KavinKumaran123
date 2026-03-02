1f8a4f0092740dc770862941dd90e4882aee45df
KavinKumaran123 <166553090+KavinKumaran123@users.noreply.github.com>
2026-03-02 00:06:45 +0000

    Practivse problem 14

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -2,5 +2,61 @@ public class PracticeProblem {
 	public static void main(String args[]) {
 
 	}
+	public static int calculate(int a, int b, char c){
+	
+		switch(c){
+			case '+':
+				return a+b;
+			case '-':
+				return a-b;
+			case '*':
+				return a*b;
+			case '/':
+				return a/b;
+			case '%':
+				return a%b;
+			case '^':
+				return (int) Math.pow(a,b);
+			default:
+				return 0;
+		}
+
+	}
+	public static String minString(String a, String b, String c) {
+
+    	String min = a;
+
+    	if (b.compareToIgnoreCase(min) < 0) {
+        	min = b;
+    	}
+
+    	if (c.compareToIgnoreCase(min) < 0) {
+        	min = c;
+    	}
+
+    	return min;
+	}
+	public static boolean totalWordsChecker(String a) {
+
+    	final int MAXIMUM = 10;
+
+    	a = a.trim();
+
+    	if (a.isEmpty()) {
+        	return true;
+    	}
+
+    	String[] words = a.split(" ");
+
+    	int count = 0;
+
+    	for (String word : words) {
+        	if (!word.isEmpty()) {
+            	count++;
+        	}
+    	}
+
+    	return count <= MAXIMUM; 
+	}
 
 }
